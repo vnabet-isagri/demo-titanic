e82cc163c97a731772bad2ae783ac86e7cc338c4
vnabet <1830490+vnabet@users.noreply.github.com>
2025-09-19 14:06:24 +0200

    amélioration de la gestion de la pagination et refacto de l'api

--- apps/backend-nitro/server/api/passengers.get.ts
+++ apps/backend-nitro/server/api/passengers.get.ts
@@ -1,5 +1,8 @@
-import { defineEventHandler } from 'h3'
+import { defineEventHandler} from 'h3'
 
+/**
+ * Type représentant les paramètres de la requête
+ */
 type Params = {
     page: string;
     limit: string;
@@ -9,29 +12,63 @@ type Params = {
     order: 'ASC' | 'DESC';
 }
 
+/**
+ * Informations de pagination
+ */
+type Pagination = {
+    page: number;
+    limit: number;
+    total: number;
+}
+
+/**
+ * Handler de la requête
+ */
 export default defineEventHandler(async (event) => {
 
     const params = getQuery(event) as Partial<Params>;
-    //console.log(params);
 
     const db = useDatabase();
 
-/*    const {rows} = await db.sql`PRAGMA table_info(passengers);`;
-console.log(rows);*/
+    const statment = db.prepare(`SELECT * from passengers` + getWhereQuery(params) + getOrderByQuery(params) + getPageQuery(params));
 
-    const statment = db.prepare(`SELECT * from passengers` + getNameQuery(params) + getSurvivedQuery(params) + getOrderByQuery(params) + getPageQuery(params));
+    const data = await statment.all() as unknown[];
 
-    return await statment.all();
+    let pagination:Partial<Pagination> | null = null;
+
+    if(data.length) {
+        pagination = await getPagination(params, db);
+        if(pagination) pagination.limit = parseInt(params?.limit ?? '');
+    }
+
+    if(pagination) {
+        return {
+            data,
+            page: pagination?.page,
+            limit: pagination?.limit,
+            total: pagination?.total
+
+        }
+    }
+    return data;
 })
 
+/**
+ * Construit lz filtre de recherche par nom
+ * @param params
+ */
 function getNameQuery(params: Partial<Params>) {
 
     if(params.name) {
-        return ` WHERE name LIKE '%${params.name}%' `;
+        return `name LIKE '%${params.name}%' `;
     }
     return '';
 }
 
+/**
+ * Construit la pagination
+ * @param params
+ */
 function getPageQuery(params: Partial<Params>) {
     if(params.page > 0 && params.limit > 0) {
         const offset = (params.page - 1) * params.limit;
@@ -40,17 +77,62 @@ function getPageQuery(params: Partial<Params>) {
     return '';
 }
 
+/**
+ * Construit le filtre de recherche sur les survivants
+ * @param params
+ */
 function getSurvivedQuery(params: Partial<Params>) {
     if(params.survived !== undefined) {
-        return params.survived === '1' ? ' AND survived = 1 ' : ' AND survived = 0 ';
+        return params.survived === '1' ? 'survived = 1 ' : ' survived = 0 ';
     }
     return '';
 }
 
+/**
+ * Construit l'ordre de la requête
+ * @param params
+ */
 function getOrderByQuery(params: Partial<Params>) {
     if(params.orderby) {
         const order = params.order === 'DESC' ? 'DESC' : 'ASC';
         return ` ORDER BY ${params.orderby} ${order} `;
     }
     return '';
+}
+
+/**
+ * Construit l'ensemble du filtre de recherche
+ * @param params
+ */
+function getWhereQuery(params: Partial<Params>) {
+    let whereQueries = [getNameQuery(params), getSurvivedQuery(params)];
+    // filtre les chaînes vides
+    whereQueries = whereQueries.filter(q => q.trim() !== '');
+
+    if(whereQueries.length) {
+        return ' WHERE ' + whereQueries.join(' AND ');
+    }
+    return '';
+}
+
+/**
+ * Récupère les infos de pagination
+ * @param params
+ * @param db
+ */
+async function getPagination(params: Partial<Params>, db:any) {
+    if(params?.page > 0 && params?.limit > 0) {
+        const offset = (params.page - 1) * params.limit;
+        const whereQuery = getWhereQuery(params);
+        const query = `SELECT FLOOR((ROW_NUMBER() OVER (ORDER BY id) - 1) / ${params.limit}) + 1 AS page,
+                                   (SELECT COUNT(*) FROM passengers ${whereQuery}) AS total
+                            FROM passengers ${whereQuery}
+                            LIMIT 1 OFFSET ${offset}`;
+
+        const statment = db.prepare(query);
+        const pagination = await statment.get();
+
+        return pagination
+    }
+    return null;
 }
